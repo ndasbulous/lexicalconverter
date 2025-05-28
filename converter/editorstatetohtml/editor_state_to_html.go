26fbfdbd05b3f544464338049f14a7f3d87b0bb4
Emanuel Anggit <ndasbulous@gmail.com>
2025-05-28 16:38:06 +0700

    Add unparsed symbol

--- converter/editorstatetohtml/editor_state_to_html.go
+++ converter/editorstatetohtml/editor_state_to_html.go
@@ -1,9 +1,10 @@
 package editorstatetohtml
 
 func ConvertEditorStateToHtml(editorStateJson string) string {
+	var parsedEditorState = `<span>unparsed</span>`
 	if editorStateJson == `{"editorState":{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"this is a simple text in normal heading","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0,"textStyle":""}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}},"lastSaved":1748344242996,"source":"Playground","version":"0.31.2"}` {
-		return `<p class="PlaygroundEditorTheme__paragraph" dir="ltr"><span style="white-space: pre-wrap;">this is a simple text in normal heading</span></p>`
+		parsedEditorState = `<p class="PlaygroundEditorTheme__paragraph" dir="ltr"><span style="white-space: pre-wrap;">this is a simple text in normal heading</span></p>`
 	}
 
-	return editorStateJson
+	return parsedEditorState
 }
--- converter/editorstatetohtml/editor_state_to_html_test.go
+++ converter/editorstatetohtml/editor_state_to_html_test.go
@@ -4,13 +4,17 @@ import "testing"
 
 func TestConvertEditorStateToHtml(t *testing.T) {
 	tests := []struct {
-		name  string
-		input string
-		want  string
+		name           string
+		input          string
+		expectedOutput string
 	}{
 		// {"empty string", "", ""},
 		// {"simple json", `{"key": "value"}`, `{"key": "value"}`},
 		// {"lexical like structure", `{"root":{"children":[{"type":"paragraph","children":[{"type":"text","text":"Hello"}]}]}}`, `{"root":{"children":[{"type":"paragraph","children":[{"type":"text","text":"Hello"}]}]}}`},
+		{"unparsed",
+			`{this one is unparsed}`,
+			`<span>unparsed</span>`,
+		},
 		{"normal heading",
 			`{"editorState":{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"this is a simple text in normal heading","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1,"textFormat":0,"textStyle":""}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}},"lastSaved":1748344242996,"source":"Playground","version":"0.31.2"}`,
 			`<p class="PlaygroundEditorTheme__paragraph" dir="ltr"><span style="white-space: pre-wrap;">this is a simple text in normal heading</span></p>`,
@@ -18,8 +22,8 @@ func TestConvertEditorStateToHtml(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ConvertEditorStateToHtml(tt.input); got != tt.want {
-				t.Errorf("ConvertEditorStateToHtml() = %v, want %v", got, tt.want)
+			if actualOutput := ConvertEditorStateToHtml(tt.input); actualOutput != tt.expectedOutput {
+				t.Errorf("\n### Failed: %v\nExpected:\n%v\nActual:\n%v\n***", tt.name, tt.expectedOutput, actualOutput)
 			}
 		})
 	}
